438f4dacf110dd4638185b8331753cb9d0dd590c
eklipsorz <eklipsorz@protonmail.ch>
2021-11-20 16:36:51 +0800

    feat: add srelloList.js and modify server settings

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-// define express, handlebars
+// define express, mongoose, handlebars
 const express = require('express')
+const mongoose = require('mongoose')
 
 
-// define port
+// define port, database_port, database_name
 const port = 3500
+const dbPort = 27017
+const dbName = 'srello-list'
 
 
 // create app with express object
 const app = express()
+
+
+// connect to mongodb
+mongoose.connect(`mongodb://localhost:${dbPort}/${dbName}`)
+
+
+// get connection between mongodb and mongoose
+const db = mongoose.connection
+
+// 
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('hi')
