2b343c98b98958d1047618a9910585cdc6a78156
eklipsorz <eklipsorz@protonmail.ch>
2021-12-22 14:41:10 +0800

    refactor: adjust login btn and register btn

--- models/users.js
+++ models/users.js
@@ -3,11 +3,14 @@ const Schema = mongoose.Schema
 
 
 const userSchema = new Schema({
+  test1: { type: String, ref: 'todo' },
+  test2: { type: Schema.Types.ObjectId, ref: 'todo' },
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 })
 
-
+console.log('test1: ', userSchema.path('test1'))
+console.log('test2: ', userSchema.path('test2'))
 exports = module.exports = mongoose.model('User', userSchema)
